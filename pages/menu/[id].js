cd546f295a25c936fd4794fe25f43edaaa8960dd
Lavenderer1375 <abedikianush@gmail.com>
2025-01-11 15:14:07 +0330

    Details of Cards done

---
+++ pages/menu/[id].js
@@ -0,0 +1,44 @@
+import DetailsPage from "@/components/templates/DetailsPage";
+import { useRouter } from "next/router";
+
+const Details = ({ data }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading the Page for You ...</div>;
+  }
+
+  return <DetailsPage {...data} />;
+};
+
+export default Details;
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3001/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+
+  const paths = data.map((food) => ({ params: { id: food.id.toString() } }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+export async function getStaticProps({ params: { id } }) {
+  // const {params: {id}} = context;
+  const res = await fetch(`http://localhost:3001/data/${id}`);
+  const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      data,
+      revalidate: 10, //seconds
+    },
+  };
+}
